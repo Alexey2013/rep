3c44def8ec9203a80d1adc4fbc56d1397c298441
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-09-20 08:47:55 +0300

    Update Header.h

--- Project4/Project4/Header.h
+++ Project4/Project4/Header.h
@@ -11,12 +11,12 @@ public:
 	Tvector(double X, double Y, double Z);
 	Tvector operator +(const Tvector& v);
 	Tvector operator -(const Tvector& v);
-	Tvector operator =(const Tvector& v);
-	double operator *(const Tvector& v);
-	double get_length();
-	double cos(Tvector& b);
+	const Tvector& operator =(const Tvector& v) ;
+	double operator *(const Tvector& v) const;
+	double get_length() const;
+	double cos(const Tvector& b) const;
 	friend ostream& operator<<(ostream& out, const Tvector& v);
-	friend istream& operator>>(istream& in,  Tvector& v);
+	friend istream& operator>>(istream& in, Tvector& v);
 };
 
 void menu();
