6e368450c9646601a02b65d99929a6d1d6b7d9eb
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-09-26 14:26:41 +0300

    Update main.cpp

--- Project5/Project5/main.cpp
+++ Project5/Project5/main.cpp
@@ -1,10 +1,10 @@
 #include  "header.h"
+#include  "header1.h"
 #include <iostream>
 using namespace std;
 
 int main() {
 	string path = get_path();
 	group group(path);
-	group.menu(path);
 	return 0;
 }
