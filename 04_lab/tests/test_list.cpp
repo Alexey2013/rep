2a025c0efc115763af3444d8f782a4da138a320f
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2024-03-07 23:08:34 +0300

    Add files via upload

--- 04_lab/tests/test_list.cpp
+++ 04_lab/tests/test_list.cpp
@@ -1,5 +1,19 @@
-#include "stack.h"
+#include "list.h"
 #include <gtest.h>
-#include <vector>
-#include <map>
+
+TEST(TList, can_create_new_list)
+{
+	ASSERT_NO_THROW(TList<int> list);
+}
+
+TEST(TList, can_create_copied_list)
+{
+	ASSERT_NO_THROW(TList<int>{new TNode<int>});
+}
+
+TEST(TList, list_is_empty_check)
+{
+	TList<int> list;
+	ASSERT_TRUE(list.IsEmpty());
+}
 
--- 04_lab/tests/test_polynom.cpp
+++ 04_lab/tests/test_polynom.cpp
@@ -1,3 +1,19 @@
-#include "stack.h"
+#include "polynom.h"
 #include <gtest.h>
+const string str;
 
+TEST(TPolynom, create_polinom)
+{
+	ASSERT_NO_THROW(TPolynom p1(str));
+}
+
+TEST(TPolynom, build_polinom)
+{
+	ASSERT_NO_THROW(TPolynom p1("4x^2y"));
+}
+
+TEST(TPolynom, create_polinom_after_existing)
+{
+	TPolynom p1("x^5z");
+	ASSERT_NO_THROW(TPolynom p2(p1));
+}
