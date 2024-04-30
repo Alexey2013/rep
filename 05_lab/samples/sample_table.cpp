c49e06e6bd6c1f6207cefcf759c1a06392cee2bd
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2024-04-30 22:45:52 +0300

    Update sample_table.cpp

--- 05_lab/samples/sample_table.cpp
+++ 05_lab/samples/sample_table.cpp
@@ -1,18 +1,3 @@
-#include <iostream>
-#include "table.h"
-#include "ScanTable.h"
-
 int main() {
-    try {
-        ScanTable<std::string, int> scan_table(100);
-        scan_table.Insert("a", new int(2));
-
-        scan_table.Insert("a", new int(5));
-        scan_table.Insert("a", new int(23));
-    }
-    catch (std::exception ex) {
-        std::cout << ex.what();
-    }
-
     return 0;
 }
